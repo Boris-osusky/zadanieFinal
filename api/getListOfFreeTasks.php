8e5804c07b6d86645e0145045c4a1ed38c789c2d
AlenBlazek <47033797+elPihi@users.noreply.github.com>
2023-05-21 12:30:41 +0200

    Update query tasks (date comparing)

--- api/getListOfFreeTasks.php
+++ api/getListOfFreeTasks.php
@@ -17,22 +17,26 @@ $studentId = $_POST['selectedValue'];
 // Get the current date
 $currentDate = date('Y-m-d');
 
-$query = "SELECT t.id, t.teacher_id 
+$query = "SELECT t.id, t.teacher_id, f.from_time, f.to_time 
           FROM tasks t
           INNER JOIN files f ON t.files_id = f.id
-          WHERE t.used != '1' 
-          AND f.from_time < '$currentDate' 
-          AND f.to_time > '$currentDate'";
+          WHERE t.used != '1'";
 
 $result = mysqli_query($connection, $query);
 
 if ($result) {
     $data = array();
     while ($row = mysqli_fetch_assoc($result)) {
-        $data[] = array(
-            'id' => $row['id'],
-            'teacher_id' => $row['teacher_id']
-        );
+
+        if($row['from_time'] < $currentDate && $row['to_time'] > $currentDate){
+
+            $data[] = array(
+                'id' => $row['id'],
+                'teacher_id' => $row['teacher_id']
+            );
+
+        }
+
     }
     $jsonData = json_encode($data);
     echo $jsonData;
